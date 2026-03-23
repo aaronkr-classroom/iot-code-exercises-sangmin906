df1e0a03415fd423f4550433d7033421116096fa
sangmin906 <psm31525@a.ut.ac.kr>
2026-03-23 11:37:36 +0900

    Add files via upload

---
+++ code/3장/3_data_types.py
@@ -0,0 +1,13 @@
+# 3_data_types.py
+a = 10
+b = 3.14
+c = "Hello"
+d = 'goodbye'
+e = True
+print(f"{a}: {type(a)}") #f: format ("서식 문자열 {변수}")
+print(f"{b}: {type(b)}")
+print(f"{c}: {type(c)}")
+print(f"{d}: {type(d)}")
+print(f"{e}: {type(e)}")
+
+print(a,":",type(a))
---
+++ code/3장/3_str.py
@@ -0,0 +1,23 @@
+# 3_str.py
+#문자열 묶는 방법
+f="hong" #큰따옴표
+n='길동' #작은따옴표
+
+#문자열 연결하기
+fn=f + n
+
+#글자수 확인
+ln=len(fn) #6
+
+#작은 따옴표와 큰 따옴표 출력하기
+qt1="It's a beautiful day"
+qt2='he said, "Hello World!"'
+qt3="She said, \"No, it's not!\""
+qt4='I\'m hungry.'
+qt5='C:\\Windows\\Users\\Desktop\\'
+
+print(1,fn)
+print("2 {ln}")
+print(3,qt1)
+print(f"4: {qt2}")
+print(f"5: {qt3}\n\t6: {qt4}\t7: {qt5}")
