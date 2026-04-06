7a04a5aef65c7eb6eabda3c192c31db1ee389e7b
sangmin906 <psm31525@a.ut.ac.kr>
2026-04-06 11:28:12 +0900

    Add files via upload

---
+++ code/5장/main.py
@@ -0,0 +1,18 @@
+#main.py
+
+from device import SensorDevice, ActuatorDevice, TemperatureSensor, LightSensor
+
+sensor = SensorDevice("Temp Sensor")
+actuator = ActuatorDevice("LED Controller")
+
+sensor.connect()
+actuator.connect()
+
+print(sensor.status())
+print(actuator.status())
+
+temp = TemperatureSensor("Temp1")
+light = LightSensor("Light1")
+
+print("Temp: ", temp.read())
+print("Light: ", light.read())
